64e5340cfa6714ee3846cecc8c9235e374c89d61
MiloszRed <308143@stud.umk.pl>
2023-05-20 11:35:43 +0200

    projekt3

---
+++ projekt3/projekt3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Distributions;
+
+namespace projekt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzenie zmiennych
+            Variable<double> probability = Variable.Beta(1, 1);
+            Range tossCount = new Range(10); // liczba rzutów
+            VariableArray<bool> coinTosses = Variable.Array<bool>(tossCount);
+
+            // Definiowanie prior dla parametru sukcesu
+            coinTosses[tossCount] = Variable.Bernoulli(probability).ForEach(tossCount);
+
+            // Określanie obserwacji
+            coinTosses.ObservedValue = new bool[] { true, true, false, true, true, true, true, true, false, true };
+
+            // Wnioskowanie
+            InferenceEngine engine = new InferenceEngine();
+            var inferredProbability = engine.Infer<Beta>(probability);
+
+            // Wyświetlanie wyników
+            System.Console.WriteLine("Mean: " + inferredProbability.GetMean());
+        }
+    }
+}

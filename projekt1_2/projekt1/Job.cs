c5341e635d55417a5080239e345755609f888f4f
MiloszRed <308143@stud.umk.pl>
2023-06-04 15:27:10 +0200

    algorytm genetyczny

---
+++ projekt1_2/projekt1/Job.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projekt1
+{
+    public class Job
+    {
+        public int Number { get; set; } // numer zadania
+        public int WorkTime { get; set; } // czas pracy
+        public int StartTime { get; set; } // czas rozpoczęcia
+        public Job NextJob { get; set; } // zadanie które może się wykonać dopiero gdy to się skończy
+        public Job PreviousJob { get; set; } // zadanie, po którego zakończeniu, to może się rozpocząć
+        public bool AssignedToProcessor { get; set; } // czy już przypisane do procesora
+        public Processor Processor { get; set;  }
+        public int IndexOnProcessor { get; set; } // indeks zadania w liście zadań procesora
+
+        // Konstruktor
+        public Job(int number, int workTime)
+        {
+            Number = number;
+            WorkTime = workTime;
+            NextJob = null;
+            PreviousJob = null;
+            AssignedToProcessor = false;
+            Processor = null;
+        }
+
+        public Job(Job job)
+        {
+            Number = job.Number;
+            WorkTime = job.WorkTime;
+            StartTime = job.StartTime;
+            NextJob = null;
+            PreviousJob = null;
+            AssignedToProcessor = true;
+            Processor = null;
+            IndexOnProcessor = job.IndexOnProcessor;
+        }
+
+        public int EndTime()
+        {
+            return StartTime + WorkTime;
+        }
+    }
+
+}
---
+++ projekt1_2/projekt1/SimulatedAnnealing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projekt1
+{
+    class SimulatedAnnealing
+    {
+        private double Temperature;
+        private double TemperatureEnd;
+        private double Alpha;
+
+        public SimulatedAnnealing(double temperature, double temperatureEnd, double alpha)
+        {
+            Temperature = temperature;
+            TemperatureEnd = temperatureEnd;
+            Alpha = alpha;
+        }
+
+        public Schedule FindBestSchedule(Schedule schedule)
+        {
+            int iterator = 0;
+
+            Random rand = new Random();
+            while (Temperature >= TemperatureEnd) // Stopping Criterion
+            {
+                // Wygeneruj nowe rozwiązanie na podstawie poprzedniego
+                Schedule newSchedule = schedule.Neighbour(rand); // Exploration Criterion
+
+                // Sprawdź czy nowe rozwiązanie jest lepsze lub zaakceptuj gorsze na podstawie funkcji akceptacji
+                if (newSchedule.GetMaxEndTime() < schedule.GetMaxEndTime() || Math.Exp((schedule.GetMaxEndTime() - newSchedule.GetMaxEndTime()) / Temperature) > rand.NextDouble()) // Acceptance Criterion
+                {
+                    schedule = newSchedule;
+                }
+
+                // Schłodzenie
+                if (iterator % 10 == 0) // Temperature Length
+                {
+                    Temperature *= Alpha; // Cooling Scheme
+                }
+                iterator++;
+            }
+
+            return schedule;
+        }
+    }
+}
